2fee06745c7f4787bdaaf68467df3ebc215c6fcb
Yooka-5 <lululomeo@gmail.com>
2019-09-02 14:34:42 +0900

    add stage

---
+++ AR_Bicycle/Bicycle/Assets/GameSceneFolder/ClickScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+using System;
+
+public class ClickScript : MonoBehaviour
+{
+    GameObject hoge;
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+        hoge = GameObject.Find("Controller");
+        GameObject.Destroy(hoge);
+        hoge = GameObject.Find("set_canvas");
+        GameObject.Destroy(hoge);
+
+        //５秒待つ
+
+        Instantiate((GameObject)Resources.Load("moveScript"));
+        //Instantiate((GameObject)Resources.Load("joystickのcanvas")); だしな、てめーのjoystick!!
+    }
+}
+
